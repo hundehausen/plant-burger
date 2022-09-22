e87f5b56ef554a789c5b3172e911efa10f612443
hundehausen <papier@grischadaum.de>
2022-09-22 21:17:20 +0200

    ramda, filter articles

--- src/pages/menu.tsx
+++ src/pages/menu.tsx
@@ -2,6 +2,7 @@ import { gql } from "graphql-request";
 import Article, { IArticle } from "../components/Article";
 import CustomHead from "../components/CustomHead";
 import { request } from "../lib/datocms";
+import { propEq, partition } from "ramda";
 
 const menuQuery = gql`
   {
@@ -46,27 +47,57 @@ export async function getStaticProps() {
 }
 
 const Menu = ({ articles }: { articles: IArticle[] }) => {
-  const burgers = articles.filter((article) => article.category === "Burger");
-  const sideDishes = articles.filter(
-    (article) => article.category === "Beilagen"
-  );
-
+  const [burgers, next] = partition(propEq("category", "Burger"), articles);
+  const [sideDishes, next2] = partition(propEq("category", "Beilagen"), next);
+  const [drinks, otherStuff] = partition(propEq("category", "Getränke"), next2);
+  console.log(otherStuff);
   return (
     <>
       <CustomHead title="Plant-Burger Speisekarte" />
       <div className="text-center">
-        <span className="text-2xl font-bold">Burger</span>
-        <div className="flex flex-row flex-wrap gap-16 p-8 justify-center">
-          {burgers.map((burger) => (
-            <Article article={burger} key={burger.title} />
-          ))}
-        </div>
-        <span className="text-2xl font-bold">Beilagen</span>
-        <div className="flex flex-row flex-wrap gap-16 p-8 justify-center">
-          {sideDishes.map((sideDish) => (
-            <Article article={sideDish} key={sideDish.title} />
-          ))}
-        </div>
+        {burgers.length > 0 && (
+          <div className="category">
+            <span className="text-2xl font-bold">Burger</span>
+            <div className="flex flex-row flex-wrap gap-16 p-8 justify-center">
+              {burgers.map((burger) => (
+                <Article article={burger} key={burger.title} />
+              ))}
+            </div>
+          </div>
+        )}
+
+        {sideDishes.length > 0 && (
+          <div className="category">
+            <span className="text-2xl font-bold">Beilagen</span>
+            <div className="flex flex-row flex-wrap gap-16 p-8 justify-center">
+              {sideDishes.map((sideDish) => (
+                <Article article={sideDish} key={sideDish.title} />
+              ))}
+            </div>
+          </div>
+        )}
+
+        {drinks.length > 0 && (
+          <div className="category">
+            <span className="text-2xl font-bold">Getränke</span>
+            <div className="flex flex-row flex-wrap gap-16 p-8 justify-center">
+              {drinks.map((drink) => (
+                <Article article={drink} key={drink.title} />
+              ))}
+            </div>
+          </div>
+        )}
+
+        {otherStuff.length > 0 && (
+          <div className="category">
+            <span className="text-2xl font-bold">Anderes</span>
+            <div className="flex flex-row flex-wrap gap-16 p-8 justify-center">
+              {otherStuff.map((otherArticle) => (
+                <Article article={otherArticle} key={otherArticle.title} />
+              ))}
+            </div>
+          </div>
+        )}
       </div>
     </>
   );
