05794078f434b0d4237ee7975aba6f73cbac474c
hundehausen <papier@grischadaum.de>
2022-09-23 22:54:55 +0200

    sort algorithm

--- src/lib/googleMapsRatings.ts
+++ src/lib/googleMapsRatings.ts
@@ -1,17 +1,28 @@
 import axios from 'axios';
+import { partition, propEq } from 'ramda';
 
 import { Review } from '../components/GoogleReviews';
 
 export const fetchGoogleReviews = async () => {
   try {
+    // fetch latest five reviews
     const response = await axios.get(
       `https://maps.googleapis.com/maps/api/place/details/json?placeid=ChIJYQ-9xsRrMQsRaG50LKHF7zI&key=${process.env.NEXT_GOOGLE_MAPS_API_TOKEN}&language=de`
     );
 
     const rawReviews: Review[] = response.data.result.reviews;
     const goodReviews = rawReviews.filter((review) => review.rating >= 3);
-    const sortedReviews = goodReviews.sort((a, b) => a.rating - b.rating);
+    const [reviewsWithoutText, reviewsWithText] = partition(
+      propEq('text', ''),
+      goodReviews
+    );
+    // if reviews with text are less than three, just take all reviews
+    const filteredReviews =
+      reviewsWithText.length < 3 ? goodReviews : reviewsWithText;
+    // sorts ascending
+    const sortedReviews = filteredReviews.sort((a, b) => a.rating - b.rating);
     if (sortedReviews.length === 5) {
+      // we only want four reviews, kick the worst review out
       sortedReviews.shift();
     }
     return sortedReviews;
